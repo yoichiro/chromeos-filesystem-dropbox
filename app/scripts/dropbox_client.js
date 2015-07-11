5b053dade9f520b81a5b765b2f88a392244a3888
Yoichiro Tanaka <yoichiro@eisbahn.jp>
2015-07-11 16:16:48 +0900

    Collected the create and delete entry functions.

--- app/scripts/dropbox_client.js
+++ app/scripts/dropbox_client.js
@@ -222,43 +222,13 @@
     };
 
     DropboxClient.prototype.createDirectory = function(directoryPath, successCallback, errorCallback) {
-        $.ajax({
-            type: "POST",
-            url: "https://api.dropbox.com/1/fileops/create_folder",
-            headers: {
-                "Authorization": "Bearer " + this.access_token_
-            },
-            data: {
-                root: "auto",
-                path: directoryPath
-            },
-            dataType: "json"
-        }).done(function(result) {
-            successCallback();
-        }.bind(this)).fail(function(error) {
-            handleError.call(this, error, successCallback, errorCallback);
-        }.bind(this));
+        createOrDeleteEntry.call(this, "create_folder", directoryPath, successCallback, errorCallback);
     };
 
     DropboxClient.prototype.deleteEntry = function(entryPath, successCallback, errorCallback) {
-        $.ajax({
-            type: "POST",
-            url: "https://api.dropbox.com/1/fileops/delete",
-            headers: {
-                "Authorization": "Bearer " + this.access_token_
-            },
-            data: {
-                root: "auto",
-                path: entryPath
-            },
-            dataType: "json"
-        }).done(function(result) {
-            successCallback();
-        }.bind(this)).fail(function(error) {
-            handleError.call(this, error, successCallback, errorCallback);
-        }.bind(this));
+        createOrDeleteEntry.call(this, "delete", entryPath, successCallback, errorCallback);
     };
-
+    
     DropboxClient.prototype.moveEntry = function(sourcePath, targetPath, successCallback, errorCallback) {
         copyOrMoveEntry.call(this, "move", sourcePath, targetPath, successCallback, errorCallback);
     };
@@ -379,6 +349,25 @@
             },
             dataType: "json"
         };
+    };
+    
+    var createOrDeleteEntry = function(operation, path, successCallback, errorCallback) {
+        $.ajax({
+            type: "POST",
+            url: "https://api.dropbox.com/1/fileops/" + operation,
+            headers: {
+                "Authorization": "Bearer " + this.access_token_
+            },
+            data: {
+                root: "auto",
+                path: path
+            },
+            dataType: "json"
+        }).done(function(result) {
+            successCallback();
+        }.bind(this)).fail(function(error) {
+            handleError.call(this, error, successCallback, errorCallback);
+        }.bind(this));
     };
 
     var handleError = function(error, successCallback, errorCallback) {
